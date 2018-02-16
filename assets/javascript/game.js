830d7dba002c335cbd862b3fd97f32d20672aa53
kyoukel <kimyoukel@gmail.com>
2018-02-16 00:17:05 -0800

    applied working js code and functions. Updated index.html to include user-guesses and display win or lose alert at the top. Still working on adding images and music and pseudocoding to explain the js coding.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,18 @@
 
 var studentName = ["Janet", "Brian", "Justin", "Ryan", "Michael", "Steven", "Robert", "Nicole", "Larry", "Kurtis", "Elizabeth"]
 
-var letters = ["a", "b", "e", "h", "i", "j", "k", "l", "m", "n", "o", "r", "s", "t", "u", "v", "y", "z"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// letters not in names: c,d,f,g,p,q,w,x
 
-var correctKeys = []
+var inCorrectKeys = []
 
 var answer;
 
 var unsolved;
+
+var level = 2;
+
+var timer =8000;
 
 // COUNTERS
 var userGuesses = 0;
@@ -21,37 +24,134 @@ var guessesLeft = 0;
 var wins = 0;
 var losses = 0;
 
-document.onkeyup = function(event) {
-var keyPressed = event.key;
-if (checkKeyPressed(keyPressed)) {
-// then add letter to word-blanks
-correctKeys.push(keyPressed)
-} else {
-  wrongGuesses += 1
-}
+document.onkeyup = function (event) {
+    var keyPressed = event.key;
+    if (letters.includes(keyPressed) && !inCorrectKeys.includes(keyPressed)) {
+
+
+        if (checkKeyPressed(answer, keyPressed)) {
+            var matches = findAllMatches(answer, keyPressed);
+            unsolved = updateInUnsolved(matches, keyPressed, unsolved)
+
+        } else {
+            wrongGuesses += 1
+            guessesLeft -= 1
+            inCorrectKeys.push(keyPressed)
+        }
+        if (unsolved.join("") === answer.toLowerCase()) youWon()
+        if (guessesLeft <= 0) youLose()
+
+        pageView()
+    }
 };
 
-function checkKeyPressed(key) {
-    // key is found in word
-    return true;
+function endGameToast(won) {
+    var newDiv = document.createElement("div");
+    if (won) {
+        newDiv.innerHTML = "CONGRATULATIONS YOU'RE A WINNER!";
+        newDiv.setAttribute("class", "alert alert-success");
+    } else {
+        newDiv.innerHTML = "Nope, Try Again!";
+        newDiv.setAttribute("class", "alert alert-danger");
+    }
+    document.getElementById("alert").appendChild(newDiv);
 }
 
-function setUp() {
+function youWon() {
+    console.log("youWon")
+    wins += 1
+    endGameToast(true)
+    setTimeout(restartGame, timer)
+}
+
+function youLose() {
+    console.log("youLost")
+    losses += 1
+    endGameToast(false)
+    setTimeout(restartGame, timer)
+}
+
+function restartGame() {
+    console.log("game restart")
+    setUp(randomIndex(studentName.length))
+    resetCounters();
+    pageView();
+}
+
+function maxGuesses(level, word) {
+    return Math.floor(word.length * level)
+}
+
+function updateInUnsolved(matches, keyPressed, unsolvedABC) {
+    var copy = Object.assign([], unsolvedABC)
+    matches.forEach(function (index) {
+        copy[index] = keyPressed
+    });
+    return copy
+}
+
+function findAllMatches(word, attempt) {
+    var wordList = word.toLowerCase().split("")
+    console.log(wordList)
+    console.log(attempt)
+    var wordMatchReducer = function (accumulator, value, index) {
+        if (value === attempt) {
+            return accumulator.concat(index)
+        } else {
+            return accumulator
+        }
+    }
+    return wordList.reduce(wordMatchReducer, [])
+}
+
+function checkKeyPressed(word, key) {
+    return word.toLowerCase().includes(key)
+}
+
+function randomIndex(length) {
+    return Math.floor(Math.random() * length)
+}
+
+// Everything below this is VIEW related
+function pageView() {
+    //replaced with below // document.querySelector("#word-blanks").innerHTML = normalizeUnsolved(unsolved)
+    updateById("#word-blanks", normalizeUnsolved(unsolved))
+    updateById("#user-guesses", normalizeUnsolved(inCorrectKeys))
+    updateById("#wrong-guesses", wrongGuesses)
+    updateById("#guesses-left", guessesLeft)
+    updateById("#win-counter", wins)
+    updateById("#loss-counter", losses)
+}
+
+
+
+function updateById(selector, input) {
+    document.querySelector(selector).innerHTML = input
+}
+
+function normalizeUnsolved(unsolved) {
+    return unsolved.join(" ")
+}
+
+function setUp(index) {
     // select random studentName index. Get student name using random index and assign to answer variable.
-    answer = studentName[Math.floor(Math.random() * studentName.length)];
-    
+    answer = studentName[index];
+
     // get # of blanks from answer
     var numOfBlanks = answer.length;
     unsolved = Array(numOfBlanks).fill("_");
     // make list with _ _ _ to length of answer.
     // reset all counters to 0.
-    resetCounters();
+
 }
-    // **code defined
+// **code defined
 function resetCounters() {
     userGuesses = 0;
     wrongGuesses = 0;
-    guessesLeft = 0;
-    wins = 0;
-    losses = 0;
+    guessesLeft = maxGuesses(level, answer);
+    inCorrectKeys = []
 }
+
+setUp(randomIndex(studentName.length))
+resetCounters();
+pageView();
---
+++ assets/javascript/test.js
@@ -0,0 +1,38 @@
+
+function assert(expected, actual, message = "Test") {
+    if (isObject(expected)) expected = JSON.stringify(expected)
+    if (isObject(actual)) actual = JSON.stringify(actual)
+    if (expected === actual) {
+        console.log('. ' + message)
+    } else {
+        console.log('X ' + message + "\n Assertion failed no match\n   expected: " + expected + "\n   actual: " + actual)
+    }
+}
+
+function assert_true(actual, message) {
+    assert(true, actual, message)
+}
+
+function assert_false(actual, message) {
+    assert(false, actual, message)
+}
+
+function isObject(value) {
+    return typeof value === "object" && value != null
+}
+
+var wrongAttempt = "c"
+var word = "book"
+var attempt = "o"
+var us = ["_", "_", "_", "_"]
+var matches = [1, 2]
+var expected = ["_", "o", "o", "_"]
+assert(expected, updateInUnsolved(matches, attempt, us))
+
+assert(["_", "m", "_", "_"], updateInUnsolved([1], "m", us))
+
+assert(matches, findAllMatches(word, attempt))
+
+assert_true(checkKeyPressed(word, attempt))
+
+assert_false(checkKeyPressed(word, wrongAttempt))
